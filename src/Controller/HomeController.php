d31240efe1b85148ff1f8c0151b5498c44dd47e4
Orlanesthesie <orlane.r.6@gmail.com>
2024-03-21 15:56:35 +0100

    push

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->render(
+            'home/index.html.twig',
+            []
+        );
+    }
+
+    #[Route('/compagny', name: 'app_compagny')]
+    public function compagny(): Response
+    {
+        return $this->render(
+            'home/compagny.html.twig',
+            []
+        );
+    }
+
+    #[Route('/loginn', name: 'app_loginn')]
+    public function login(): Response
+    {
+        return $this->render(
+            'home/login.html.twig',
+            []
+        );
+    }
+
+    #[Route('/registerr', name: 'app_registerr')]
+    public function registerr(): Response
+    {
+        return $this->render(
+            'home/registerr.html.twig',
+            []
+        );
+    }
+
+    #[Route('/contact', name: 'app_contact')]
+    public function contact(): Response
+    {
+        return $this->render(
+            'home/contact.html.twig',
+            []
+        );
+    }
+
+    #[Route('/profilee', name: 'app_profilee')]
+    public function profilee(): Response
+    {
+        return $this->render(
+            'home/profilee.html.twig',
+            []
+        );
+    }
+}
